b9266d3c09f26d62fa257e514430d367a7caad59
Miguel Avilés <uca@uca.edu>
2018-05-12 19:03:51 -0600

    APP menu desplegable, con opciones que cambian el color de la pantalla y muestra un login

---
+++ app/src/main/java/com/meag/navigationdrawer/BlueFragment.java
@@ -0,0 +1,31 @@
+package com.meag.navigationdrawer;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlueFragment extends Fragment {
+
+    FrameLayout layoutblue;
+    public BlueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.fragment_blue, container, false);
+
+        return v;
+    }
+
+}
